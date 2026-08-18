35e03140587c21fed83e67135296d2d50e2e2ba7
Sherry <shehryarr2002@gmail.com>
2026-08-18 08:48:07 +0500

    Final Full

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function proxy(request) {
   const url = request.nextUrl;
 
   // Check if the request is for the admin route
